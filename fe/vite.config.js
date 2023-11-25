02e72632f26e38f5471472e89a7b28796d7acce3
Nguyen Anh Tuan <nguyenanhtuan19122002@gmail.com>
2023-11-25 19:52:47 +0700

    Update: fix build tailwindcss

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -1,9 +1,10 @@
+import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: [
       { find: "src", replacement: "/src" }
